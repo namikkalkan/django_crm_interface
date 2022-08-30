a24e098163b6d08ba7bba6f57bf1ea500610a301
Namik <53353808+namikkalkan@users.noreply.github.com>
2022-08-30 12:11:30 +1000

    Update settings.py

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(4%xz9bbi-$c*9jelwllef$7k9jikej)dzbhy-vb(h*_*!!ey@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://djangocrminterface-production.up.railway.app/']
+ALLOWED_HOSTS = ['djangocrminterface-production.up.railway.app']
 
 
 # Application definition
